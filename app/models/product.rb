534563f80b79aada2b10eadcda82d14c0ad4dd2e
Sondetsu <sondetsu@gmail.com>
2018-11-22 12:18:42 -0600

    Updated the image uploader and successfully uploaded an image.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,7 @@ class Product < ApplicationRecord
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
 
+  mount_uploader :image, ImageUploader
+
   validates :title, :price, presence: true
 end
