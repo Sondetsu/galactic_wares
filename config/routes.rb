e50c9a0ee393d8a3553b0ff0ef1c1b0d6f870bc0
Sondetsu <sondetsu@gmail.com>
2018-11-26 16:37:19 -0600

    Adjusted the routes for product index and show. Set the root to the products index. Will likely be changed later.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
+  resources :products, only: %i[index show]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'products#index'
 end
