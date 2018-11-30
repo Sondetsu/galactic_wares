4b833b60a76ff9559f7559ff88e336b5c9ab84c5
Sondetsu <sondetsu@gmail.com>
2018-11-30 03:35:27 -0600

    Added a search bar to the products index view.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,5 @@
 class SearchController < ApplicationController
+    def results
+        @products = Product.where('title LIKE ?', "%#{params[:q]}%")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@
 
 Rails.application.routes.draw do
   resources :products, only: %i[index show]
+  
+  resources :search, only: [:index] do
+    collection do
+      get 'results'
+    end
+  end
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
