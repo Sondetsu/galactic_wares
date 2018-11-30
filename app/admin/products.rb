19df7c409cff79380717fc417ba3e2c86cb99edf
Sondetsu <sondetsu@gmail.com>
2018-11-30 02:22:41 -0600

    Modified active admin to allow many to many relationship and add categories to products.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -14,5 +14,47 @@ ActiveAdmin.register Product do
   #   permitted
   # end
 
-  permit_params :title, :description, :price, :image, :category
+  permit_params :title, :description, :price, :image, product_categories_attributes: [:id, :product_id, :category_id, :_destroy]
+
+  index do
+    selectable_column
+    column :id
+    column :title
+    column :description
+    column :price
+    column :image
+    column :categories do |product|
+      product.categories.map { |p| p.title }.join(", ").html_safe
+    end
+    actions
+  end
+
+  show do |product|
+    attributes_table do
+      row :title
+      row :description
+      row :price
+      row :image
+      row :categories do |product|
+        product.categories.map { |p| p.title }.join(", ").html_safe
+      end
+    end
+  end
+
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+
+    f.inputs "Product" do
+      f.input :title
+      f.input :description
+      f.input :price
+      f.input :image
+      f.has_many :product_categories, allow_destroy: true do |n_f|
+        n_f.input :category
+      end
+    end
+
+    f.actions
+  end
 end
+
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@
 class Product < ApplicationRecord
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
+  accepts_nested_attributes_for :product_categories, allow_destroy: true
 
   mount_uploader :image, ImageUploader
 
