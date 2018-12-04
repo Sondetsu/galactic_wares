16a0d0e6d990285877c3c07eec716baf9a56dbc7
Sondetsu <sondetsu@gmail.com>
2018-12-04 02:57:53 -0600

    Resized the images manually and added pagination for the search results.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,9 +4,13 @@ class SearchController < ApplicationController
   def results
     if params[:search][:category].present?
       @category = Category.find(params[:search][:category])
-      @products = @category.products.where('title LIKE ?', "%#{params[:q]}%")
+      @products = @category.products.where('title LIKE ?', "%#{params[:q]}%").order('title').page(params[:page]).per(5)
+
+      # This is to get the proper results size, despite pagination.
+      @count = @category.products.where('title LIKE ?', "%#{params[:q]}%").size
     else
-      @products = Product.where('title LIKE ?', "%#{params[:q]}%")
+      @products = Product.where('title LIKE ?', "%#{params[:q]}%").order('title').page(params[:page]).per(5)
+      @count = Product.where('title LIKE ?', "%#{params[:q]}%").order('title').size
     end
   end
 end
